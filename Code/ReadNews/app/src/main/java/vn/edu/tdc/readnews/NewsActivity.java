a6262716615216d9fe18955d37406cc236b5fd31
Vu Dinh Khanh <vudinhkhanh2810@github.com>
2018-05-08 14:38:59 +0700

    [khanh] commit main code of project

---
+++ Code/ReadNews/app/src/main/java/vn/edu/tdc/readnews/NewsActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.tdc.readnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_layout);
+    }
+}
